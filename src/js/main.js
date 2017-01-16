84e4bf93206f06da20094479e21dee69f4151069
DaniniSC <dpsantandercastro@gmail.com>
2017-01-16 14:14:02 -0300

    Variable categorías

---
+++ src/js/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+	var categorias = [
+		{
+			name: "Aire Libre-Parrillas",
+			img: ""
+		},
+		{
+			name: "Automóvil",
+			img: ""
+		},
+		{
+			name: "Baño",
+			img: ""
+		},
+		{
+			name: "Cocina",
+			img: ""
+		},
+		{
+			name: "Construcción",
+			img: ""
+		},
+		{
+			name: "Decohogar",
+			img: ""
+		},
+		{
+			name: "Electro y Línea Blanca",
+			img: ""
+		},
+		{
+			name: "Ferretería",
+			img: ""
+		},
+		{
+			name: "Herramientas",
+			img: ""
+		},
+		{
+			name: "Iluminación",
+			img: "",
+			subcategoria: [
+				{
+					name_sc: "Ampolletas y Fluorescencia",
+					img_sc: ""
+				},
+				{
+					name_sc: "Iluminación Exterior",
+					img_sc: ""
+				},
+				{
+					name_sc: "Iluminación Interior",
+					img_sc: ""
+				},
+				{
+					name_sc: "Iluminación LED",
+					img_sc: ""
+				},
+				{
+					name_sc: "Ventiladores de techo",
+					img_sc: ""
+				},
+				{
+					name_sc: "Iluminación para tu negocio",
+					img_sc: ""
+				},
+				{
+					name_sc: "Automatización del Hogar",
+					img_sc: ""
+				}
+			]
+		},
+		{
+			name: "Jardín y terrazas",
+			img: ""
+		},
+		{
+			name: "Muebles",
+			img: ""
+		},
+		{
+			name: "Pinturas",
+			img: ""
+		},
+		{
+			name: "Pisos y cerámicas",
+			img: ""
+		},
+		{
+			name: "Puertas y ventanas",
+			img: ""
+		}
+	];
+});
