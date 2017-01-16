3d3c35e1c23346fa56d88a120ad6df04ef1c99f5
DaniniSC <dpsantandercastro@gmail.com>
2017-01-16 19:13:43 -0300

    Se agregan íconos en los arreglos

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,46 @@
 $(document).ready(function(){
+
+	//arreglo de objetos con categorías, ccc y sub categorías
 	var categorias = [
 		{
 			name: "Aire Libre-Parrillas",
-			img: ""
+			img: "../../dist/img/airelibre_y_parrilla.png"
 		},
 		{
 			name: "Automóvil",
-			img: ""
+			img: "../../dist/img/automovil.png"
 		},
 		{
 			name: "Baño",
-			img: ""
+			img: "../../dist/img/bano.png"
 		},
 		{
 			name: "Cocina",
-			img: ""
+			img: "../../dist/img/cocina.png"
 		},
 		{
 			name: "Construcción",
-			img: ""
+			img: "../../dist/img/construccion.png"
 		},
 		{
 			name: "Decohogar",
-			img: ""
+			img: "../../dist/img/decohogar.png"
 		},
 		{
 			name: "Electro y Línea Blanca",
-			img: ""
+			img: "../../dist/img/electronica_lineablanca.png"
 		},
 		{
 			name: "Ferretería",
-			img: ""
+			img: "../../dist/img/ferreteria.png"
 		},
 		{
 			name: "Herramientas",
-			img: ""
+			img: "../../dist/img/herramientas.png"
 		},
 		{
 			name: "Iluminación",
-			img: "",
+			img: "../../dist/img/iluminacion.png",
 			subcategoria: [
 				{
 					name_sc: "Ampolletas y Fluorescencia",
@@ -46,39 +48,39 @@ $(document).ready(function(){
 					subsgtecategoria: [
 					{
 						name_ssc: "Ampolletas LED",
-						img: ""
+						img_ssc: ""
 					},
 					{
 						name_ssc: "Ampolletas Ahorro de Energía",
-						img: ""
+						img_ssc: ""
 					},
 					{
 						name_ssc: "Ampolletas Halógenas",
-						img: ""
+						img_ssc: ""
 					},
 					{
 						name_ssc: "Ampolletas Incandescentes",
-						img: ""
+						img_ssc: ""
 					},
 					{
 						name_ssc: "Ampolletas Eco Halógenas",
-						img: ""
+						img_ssc: ""
 					},
 					{
 						name_ssc: "Ampolletas Vintage Edison",
-						img: ""
+						img_ssc: ""
 					},
 					{
-						name_ssc: "Flourescencia Compacta",
-						img: ""
+						name_ssc: "Fluorescencia Compacta",
+						img_ssc: ""
 					},
 					{
 						name_ssc: "Fluorescentes Circulares",
-						img: ""
+						img_ssc: ""
 					},
 					{
 						name_ssc: "Tubos Fluorescente",
-						img: ""
+						img_ssc: ""
 					}]
 				},
 				{
@@ -109,23 +111,153 @@ $(document).ready(function(){
 		},
 		{
 			name: "Jardín y terrazas",
-			img: ""
+			img: "../../dist/img/jardin_terraza.png"
 		},
 		{
 			name: "Muebles",
-			img: ""
+			img: "../../dist/img/muebles.png"
 		},
 		{
 			name: "Pinturas",
-			img: ""
+			img: "../../dist/img/pinturas.png"
 		},
 		{
 			name: "Pisos y cerámicas",
-			img: ""
+			img: "../../dist/img/pisos_ceramica.png"
 		},
 		{
 			name: "Puertas y ventanas",
-			img: ""
+			img: "../../dist/img/puertas_ventanas.png"
 		}
 	];
+
+	//funciones para quitar display=none en cada caso;
+	//aire libre-parrillas
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//automovil
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//baño
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//cocina
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//construccion
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//decohogar
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});//electro y linea blanca
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//ferreteria
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//herramientas
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//iluminacion
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+		//sc ampolletas y fluorescencia
+		$('').on('click', function(){
+			$('').removeClass('ocultar');
+			//ssc ampolletas led
+			$('').on('click', function(){
+				$('').removeClass('ocultar');
+			});
+			//ssc ampolletas ahorro de energia
+			$('').on('click', function(){
+				$('').removeClass('ocultar');
+			});
+			//ssc ampolletas halogenas
+			$('').on('click', function(){
+				$('').removeClass('ocultar');
+			});
+			//ssc ampolletas incandescentes
+			$('').on('click', function(){
+				$('').removeClass('ocultar');
+			});
+			//ssc ampolletas eco halogenas
+			$('').on('click', function(){
+				$('').removeClass('ocultar');
+			});
+			//ssc ampolletas vintage edison
+			$('').on('click', function(){
+				$('').removeClass('ocultar');
+			});
+			//ssc fluorescencia compacta
+			$('').on('click', function(){
+				$('').removeClass('ocultar');
+			});
+			//ssc fluorescentes circulares
+			$('').on('click', function(){
+				$('').removeClass('ocultar');
+			});
+			//ssc tubos fluorescentes
+			$('').on('click', function(){
+				$('').removeClass('ocultar');
+			});
+		});
+		//sc iluminacion exterior
+		$('').on('click', function(){
+			$('').removeClass('ocultar');
+		});
+		//sc iluminacion interior
+		$('').on('click', function(){
+			$('').removeClass('ocultar');
+		});
+		//sc iluminacion led
+		$('').on('click', function(){
+			$('').removeClass('ocultar');
+		});
+		//sc ventiladores de techo
+		$('').on('click', function(){
+			$('').removeClass('ocultar');
+		});
+		//sc iluminacion para tu negocio
+		$('').on('click', function(){
+			$('').removeClass('ocultar');
+		});
+		//sc automatizacion del hogar
+		$('').on('click', function(){
+			$('').removeClass('ocultar');
+		});
+	});
+	//jardin y terrazas
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//muebles
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//pinturas
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//pisos y ceramicas
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+	//puertas y ventanas
+	$('').on('click', function(){
+		$('').removeClass('ocultar');
+	});
+
+	//guardar datos al dar click en buscar
+	$('#botonBuscar').on('click', function (){
+
+	})
 });
