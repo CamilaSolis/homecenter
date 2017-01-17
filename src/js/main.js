41e8038ac64ff7310ce1bc9ed92b9b9c4cbe4fb2
polilla alarcon <polilla alarcon>
2017-01-17 16:11:54 -0300

    tomar en cuenta solo los archivos que estan en la rama estos serian main js main css testingajax

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+/*var a = function(sku){
+		$.ajax({
+			url: 'http://api-car.azurewebsites.net:80/Categories/CL/66/cat3390003',
+			type: 'GET',
+			datatype: 'json',
+			data : {
+				q : sku
+			}
+		})
+		.done(function(response) {
+			console.log(response);
+		})
+		.fail(function() {
+			console.log("error");
+		});
+	}*/
